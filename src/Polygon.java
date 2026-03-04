00e8c2dc8af7e8d5ae59ac9a2a5b95961e534708
Bartosz W <bartoszwozniak123@gmail.com>
2026-03-04 18:20:40 +0100

    Zadanie 4

--- src/Polygon.java
+++ src/Polygon.java
@@ -6,6 +6,11 @@ public class Polygon
         this.points = new Point[points.length];
         for (int i = 0; i < points.length; i++) this.points[i] = new Point(points[i]);
     }
+    public Polygon(Polygon p)
+    {
+        points = new Point[p.points.length];
+        for (int i = 0; i < points.length; i++) points[i] = new Point(p.points[i]);
+    }
     public String toString()
     {
         String out = "{";
